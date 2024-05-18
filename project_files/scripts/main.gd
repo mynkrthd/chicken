214aa00c87d06ed1a6b48e169cc205c2eb3950b9
mynkrthd <81139504+MayankDirtcube@users.noreply.github.com>
2024-05-18 16:30:04 +0530

    puase menu

--- project_files/scripts/main.gd
+++ project_files/scripts/main.gd
@@ -7,7 +7,12 @@ extends Node2D
 @onready var hud = $CanvasLayer/hud
 @onready var game_over = $CanvasLayer/GameOver
 
-
+func gamePuase():
+	pause_menu.show()
+	
+func gameResume():
+	pause_menu.hide()
+	
 func _ready():
 	main_menu.show()
 	pause_menu.hide()
@@ -15,10 +20,9 @@ func _ready():
 	hud.hide()
 	player_chicken.hide()
 	
-
-	
-
-
+func _input(event):
+	if(event.is_action_pressed("pause_game")):
+		gamePuase()
 func _on_main_menu_startgame():
 	player_chicken.show()
 	hud.show()
@@ -27,3 +31,6 @@ func _on_main_menu_startgame():
 
 func _on_main_menu_quitgame():
 	get_tree().quit()
+
+func _on_pause_menu_resumegame():
+	gameResume()
